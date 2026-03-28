9d196243d8648d2347035febbd583b14ffefa23d
iiivanpopov <iiivanpopov999@gmail.com>
2026-03-28 18:27:05 +0000

    Init

---
+++ packages/http/tsdown.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: 'src/index.ts',
+  format: 'esm',
+  clean: true,
+  outExtensions: () => ({ js: '.js', dts: '.d.ts' }),
+  dts: { tsgo: true },
+})
