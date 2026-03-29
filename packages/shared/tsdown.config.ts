5aebf3337ee27fe77c6d8bee86061b3215f62c51
iiivanpopov <iiivanpopov999@gmail.com>
2026-03-29 18:16:03 +0000

    chore

--- packages/shared/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  entry: 'src/index.ts',
-  format: 'esm',
-  clean: true,
-  outExtensions: () => ({ js: '.js', dts: '.d.ts' }),
-  dts: { oxc: true },
-})
