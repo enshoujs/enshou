49046f3e82894da254c706745bdab5c0c6d24a2d
rhyei <298654965+rhyei@users.noreply.github.com>
2026-07-11 14:18:35 +0000

    🍥 0.8.8

--- packages/di/src/token.ts
+++ packages/di/src/token.ts
@@ -4,7 +4,9 @@ import { isClass } from '#shared/utils'
 
 export type Token<Target = unknown> = symbol & { __for: Target }
 
-export function token<Target = Class<any>>(target: Class<any> | string): Token<Target> {
-  if (isClass(target)) return Symbol(target.name) as Token<Target>
-  return Symbol(target) as Token<Target>
+export function token<Target>(name: string): Token<Target>
+export function token<Target>(target: Class<Target>): Token<Target>
+export function token(target: any): any {
+  if (isClass(target)) return Symbol(target.name)
+  return Symbol(target)
 }
